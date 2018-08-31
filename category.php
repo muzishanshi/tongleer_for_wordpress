1d7ad742820a6b9395f96340a07156f2fd1d96f8
muzishanshi <muzishanshi@126.com>
2018-08-31 20:50:09 +0800

    Diamond

--- category.php
+++ category.php
@@ -248,49 +248,7 @@
 	  <?php
 	  }else{
 	  ?>
-		<style>
-		.page-main{
-			background-color:#fff;
-			margin:0px auto 0px auto;
-		}
-		@media screen and (max-width: 960px) {
-			.page-main {width: 100%;}
-		}
-		</style>
-		<section class="page-main">
-		  <div class="admin-content">
-			<div class="admin-content-body">
-			  <div class="am-cf am-padding am-padding-bottom-0">
-				<div class="am-fl am-cf"><strong class="am-text-primary am-text-lg">404</strong> / <small>That’s an error</small></div>
-			  </div>
-
-			  <hr>
-
-			  <div class="am-g">
-				<div class="am-u-sm-12">
-				  <h2 class="am-text-center am-text-xxxl am-margin-top-lg">404. Not Found</h2>
-				  <p class="am-text-center">没有找到你要的页面</p>
-				<pre class="page-404">
-				  .----.
-			   _.'__    `.
-		   .--($)($$)---/#\
-		 .' @          /###\
-		 :         ,   #####
-		  `-..__.-' _.-\###/
-				`;_:    `"'
-			  .'"""""`.
-			 /,  ya ,\\
-			//  404!  \\
-			`-._______.-'
-			___`. | .'___
-		   (______|______)
-				</pre>
-				</div>
-			  </div>
-			</div>
-		  </div>
-		<!-- content end -->
-		</section>
+		<?php include "templates/404_include.php";?>
 	  <?php
 	  }
 	  ?>  
---
+++ templates/404_include.php
@@ -0,0 +1,28 @@
+<style>
+.page-main{
+	background-color:#fff;
+	width:100%;
+	margin:0px auto 0px auto;
+}
+@media screen and (max-width: 960px) {
+	.page-main {width: 100%;}
+}
+</style>
+<!-- content start -->
+<section class="page-main">
+  <div class="admin-content">
+    <div class="admin-content-body">
+      <div class="am-g">
+        <div class="am-u-sm-12">
+          <h2 class="am-text-center am-text-xxxl am-margin-top-lg">404. Not Found</h2>
+          <p class="am-text-center">没有找到你要的页面</p>
+        <pre class="page-404">
+        
+        </pre>
+        </div>
+      </div>
+    </div>
+
+  </div>
+<!-- content end -->
+</section>
